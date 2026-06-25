8b97557645fa4c610553be0fa173262f64c29326
whambamtattoo <whambamtattoo@gmail.com>
2026-06-25 18:40:05 +0100

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,7 @@
 import { products } from '../../lib/products'
 
 export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.whambamtattoo.com')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
