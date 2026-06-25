026b609c2f7397ea2e6df520a4541faf0b3bfddc
whambamtattoo <whambamtattoo@gmail.com>
2026-06-25 18:39:27 +0100

    Update checkout.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -11,7 +11,7 @@ const SHIPPING_RATES = {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.whambamtattoo.com')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
