29a1a7259bf78bf5d4c443e7d1dac5509b26db09
whambamtattoo <whambamtattoo@gmail.com>
2026-07-24 17:46:52 +0100

    Update availability.js

--- pages/api/availability.js
+++ pages/api/availability.js
@@ -7,6 +7,8 @@ const CALENDAR_IDS = [
 ];
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
